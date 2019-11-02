921a5fdea048ce8fc755b8e667fbe934e5eccdb1
Pablo <pablo.duran.online@gmail.com>
2019-11-02 10:29:41 +0100

    initial commit

---
+++ Scripts/easy_install-3.7-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\pablo\Desktop\programming\the_sum_of_two\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.7'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install-3.7')()
+    )
---
+++ main.py
@@ -0,0 +1,9 @@
+def sum(a,b):
+    result = a + b
+    return result
+
+print(sum(3, 89))
+print(sum(4, 78))
+print(sum(89, 23))
+
+
